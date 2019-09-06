0b016bd0b9687af1f4d1b99e3f8369daa6521687
Patrick Lohan <patrick.lohan@zoho.com>
2019-09-06 12:58:15 +0100

    Fixed VueMap template typo

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <Map msg="Welcome to Your Vue.js App"/>
+    <VueMap msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
