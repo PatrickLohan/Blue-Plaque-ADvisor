6fc5c82170811a2b63c40f0df8908db28a9598f3
Allan Brown <allanbrown8362@gmail.com>
2019-09-09 15:39:43 +0100

    added subjects name into data form

--- client/src/components/UserUpdatePlaque.vue
+++ client/src/components/UserUpdatePlaque.vue
@@ -2,8 +2,9 @@
 <div >
   <form class="updateForm" v-on:submit.prevent="addLocation">
     <input type="text" v-model="title" placeholder="Plaque Title"required>
+    <input type="text" v-model="subjects" placeholder="Name of Subject">
     <input type="text" v-model="colour_name" placeholder="Colour"required>
-    <input type="text" v-model="inscription" placeholder="Inscription">
+    <input type="text" v-model="inscription" placeholder="Inscription" required>
     <input type="text" v-model="address" placeholder="Address">
     <input type="submit" value="Submit">
   </form>
@@ -20,6 +21,7 @@ export default {
   data(){
     return {
       title: '',
+      subjects: '',
       colour_name: '',
       inscription: '',
       address: ''
@@ -29,6 +31,7 @@ export default {
   methods: {
     addLocation(location){
       this.userLocation["title"]=this.title
+      this.userLocation["subjects"]=this.subjects
       this.userLocation["colour_name"]=this.colour_name
       this.userLocation["inscription"]=this.inscription
       this.userLocation["address"]=this.address
