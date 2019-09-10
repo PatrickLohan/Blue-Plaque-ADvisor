c2ee39a54b8f41a2f915d30ccae2483aeee6f28c
Patrick Lohan <patrick.lohan@zoho.com>
2019-09-10 21:00:39 +0100

    Adds github links, changes favourites icon for consistency

--- client/src/components/FooterBar.vue
+++ client/src/components/FooterBar.vue
@@ -1,6 +1,10 @@
 <template lang="html">
   <div id="footer-bar">
-    <p>footer dummy text</p>
+    <p>Github links: </p>
+    <a href="https://github.com/bmac110">Billy</a>
+    <a href="https://github.com/PadraigLeoghain">Patrick</a>
+    <a href="https://github.com/allan8362">Allan</a>
+    <a href="https://github.com/DianeEugenie">Diane</a>
   </div>
 </template>
 
@@ -10,10 +14,14 @@ export default {
 </script>
 
 <style lang="css" scoped>
-
+*{
+  font-size: 10px;
+}
 #footer-bar{
   background-color: #FF903F;
   padding: 10px;
-  text-align: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
 }
 </style>
