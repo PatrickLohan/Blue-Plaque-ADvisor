194ea74f2ae94686162fa96713cd1fbb17f16790
Patrick Lohan <patrick.lohan@zoho.com>
2019-09-10 10:55:35 +0100

    Fixed missing method and element

--- client/src/components/PlaqueDetails.vue
+++ client/src/components/PlaqueDetails.vue
@@ -7,6 +7,7 @@
       <h3 v-on:click="addFavourite" v-if="!this.favourites.includes(location)">Add to Favourites</h3>
       <h3 v-on:click="removeFavourite" v-if="this.favourites.includes(location)">Remove Favourite</h3>
       <!-- //Can we just open new tab with uri/url within the 'html' element or do we need a function? -->
+      <h3 v-on:click="updateLocation">Update Details</h3>
       <h3 v-on:click="goWiki">More Info!</h3>
       <h3 v-on:click="goToLocation" v-if="location.latitude || location.longitude">Get Me Here</h3>
       <h4 v-if="!location.latitude || !location.longitude">Oh no! Please add coordinates</h4>
@@ -34,6 +35,10 @@ export default {
     goToLocation: function(location) {
       let endLocation = [this.location.latitude, this.location.longitude];
       eventBus.$emit('route-end', endLocation);
+    },
+    updateLocation: function(location){
+      eventBus.$emit('update-location', this.location);
+      eventBus.$emit('option-selected', 'update');
     }
   }
 }
