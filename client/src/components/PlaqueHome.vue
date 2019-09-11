206fe09d6a8b81d7630805c225d3a5135cc07a4c
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-09-11 13:57:06 +0100

    Adjusts homepage text

--- client/src/components/PlaqueHome.vue
+++ client/src/components/PlaqueHome.vue
@@ -5,23 +5,26 @@
       You may have noticed plaques without realizing what they are.
     </p>
     <p>
-      What is a plaque? Why are they placed where they are?
+      Our app takes you on a historic adventure through Glasgow discovering all the plaques, not just the blue ones.
     </p>
     <p>
-      Well, a plaque is a sign that is installed in a public place to commemorate a historical connection between that location, a significant event or a famous person. It is a marker of history! You can put up a site for plaque nomination in Scotland through <a href="https://www.historicenvironment.scot/about-us/what-we-do/commemorative-plaque-scheme/#overview_tab">Historic Environment Scotland</a>. You can't nominate yourself though, one of the criteria is that you have to be dead for at least 20 years!
+      If you notice a plaque that is not on the map, just <b>double click</b> to add a new location, fill in the details in the form and voil&agrave;, a new entry has been made!
     </p>
     <p>
       We have used the amazing database of <a href="https://openplaques.org/">Open Plaques</a> which is a community-based project made up of volunteers who go out and about to discover, curate and catalogue commemorative plaques and historical markers throughout the world.
     </p>
+    <h4>
+      What is a plaque? Why are they placed where they are?
+    </h4>
     <p>
-      Our app takes you on a historic adventure through Glasgow discovering all the plaques, not just the blue ones.
+      Well, a plaque is a sign that is installed in a public place to commemorate a historical connection between that location, a significant event or a famous person. It is a marker of history! You can put up a site for plaque nomination in Scotland through <a href="https://www.historicenvironment.scot/about-us/what-we-do/commemorative-plaque-scheme/#overview_tab">Historic Environment Scotland</a>. You can't nominate yourself though, one of the criteria is that you have to be dead for at least 20 years!
     </p>
+
+
     <p class="p-blue">
       Time to Explore!
     </p>
-    <p>
-      By the way! If you notice one that is not on the map? Just double click the location, fill in the details in the form and voil&agrave;, a new entry has been made!
-    </p>
+
   </p>
 </div>
 </template>
@@ -34,7 +37,7 @@ export default {
 
 <style lang="css" scoped>
 
-h3, .p-blue {
+h3, h4, .p-blue {
   color: #477CDE;
 }
 a {
